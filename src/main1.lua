22b7d55c81b8d74186398fdc457d71b5b23adf8e
Shamil Gumirov <shamil@gumirov.com>
2016-12-22 17:58:11 +0700

    added print

--- src/main1.lua
+++ src/main1.lua
@@ -9,12 +9,13 @@ local function main_tcp()
   local ip, port = server:getsockname()
   -- print a message informing what's up
   print("Please telnet to localhost on port " .. port)
-  print("After connecting, you have 10s to enter a line to be echoed")
+--  print("After connecting, you have 10s to enter a line to be echoed")
   local con = 0 --counter
   -- loop forever waiting for clients
   while 1 do
     -- wait for a connection from any client
     local client = server:accept()
+    print("Server: connection accepted")
     
     local handler = function(client)
       -- make sure we don't block waiting for this client's line
